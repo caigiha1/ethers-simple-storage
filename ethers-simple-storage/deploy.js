5f1a5edab2812f0f20c48df6897d47fefdfc6ef1
cagiha1 <tranminhquyet13@gmail.com>
2022-09-22 11:43:13 +0700

    config env privatekey and rpcurl

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -1,18 +1,14 @@
 const ethers = require('ethers');
 const fs = require('fs-extra');
+require('dotenv').config();
 
 async function main() {
   // compile them in our code
   // compile them
   // HTTP://127.0.0.1:7545
-  const provider = new ethers.providers.JsonRpcProvider(
-    'HTTP://172.25.32.1:7545'
-  );
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 
-  const wallet = new ethers.Wallet(
-    'e4f31fbdb51be49bc90ad22ef11f63529077c631692b70ca1b9707d6c7912096',
-    provider
-  );
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8');
   const binary = fs.readFileSync(
     './SimpleStorage_sol_SimpleStorage.bin',
@@ -24,8 +20,16 @@ async function main() {
   console.log('Deploying, please wait...');
 
   const contract = await contractFactory.deploy(); // Stop here! wait for contract deploy
+  await contract.deployTransaction.wait(1);
 
-  console.log(contract);
+  const currentFavoriteNumber = await contract.retrieve();
+  console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
+
+  const transactionResponse = await contract.store('7');
+  const transactionReceipt = await transactionResponse.wait(1);
+
+  const updatedFavoriteNumber = await contract.retrieve();
+  console.log(`Updated favorite number is: ${updatedFavoriteNumber}`);
 }
 
 main()
